08ec01db1ce9cb524329dd516766e7e74ca645d2
flitscha <felixcampidell@gmail.com>
2025-05-24 22:18:07 +0200

    basic game loop

---
+++ src/game.py
@@ -0,0 +1,24 @@
+import pygame
+
+from settings import *
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption(GAME_NAME)
+        self.clock = pygame.time.Clock()
+        self.running = True
+        
+
+    def run(self):
+        
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.screen.fill((0, 0, 0))
+            pygame.display.flip()
+
+        pygame.quit()
---
+++ src/settings.py
@@ -0,0 +1,4 @@
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+GAME_NAME = "Theorem Factory"
